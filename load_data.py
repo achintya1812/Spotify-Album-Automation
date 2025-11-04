f72013a7abd2a6daf39ab22573206cca22ab3337
achintya1812 <achintyamathur18@gmail.com>
2025-11-04 13:56:41 +0530

    switched to CSV format for data

---
+++ load_data.py
@@ -0,0 +1,39 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+import os
+from dotenv import load_dotenv
+
+import csv
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = client_id,
+                                               client_secret = client_secret,
+                                               redirect_uri = "http://127.0.0.1:8888",
+                                               scope = "user-library-read"))
+
+results = sp.current_user_saved_tracks()
+tracks = results["items"]
+total = results["total"]
+
+while results["next"]:
+    results = sp.next(results)
+    tracks.extend(results["items"])
+    print(f"Progress: {len(tracks)}/{total}")
+
+with open("liked_songs.csv", "w", newline = "", encoding = "utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["track", "artist", "album"])
+
+    for item in tracks:
+        track = item["track"]
+        if track:
+            writer.writerow([
+                track['name'],
+                track['artists'][0]['name'],
+                track['album']['name']
+            ])
--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import csv 
 
-import os
-from dotenv import load_dotenv
-
-import json
-
-load_dotenv()
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = client_id,
-                                               client_secret = client_secret,
-                                               redirect_uri = "http://127.0.0.1:8888",
-                                               scope = "user-library-read"))
-
-results = sp.current_user_saved_tracks()
-tracks = results["items"]
-
-while results["next"]:
-    results = sp.next(results)
-    tracks.extend(results["items"])
-
-for index, item in enumerate(tracks):
-    track = item["track"]
-    print(index, track["artists"][0]["name"], " - ", track["name"])
-
-with open("liked_songs.json", "w") as file:
-    json.dump(tracks, file, indent = 2)
+with open("liked_songs.csv", "r") as file:
+    data = csv.reader(file, delimiter=",")
+    
